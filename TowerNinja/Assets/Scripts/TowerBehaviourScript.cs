b5ec3722fe34d1dae2be0f99d8f85ac4c7f7222f
sway2020 <ysui@usc.edu>
2021-06-03 20:52:09 -0700

    Update TowerBehaviourScript.cs

--- TowerNinja/Assets/Scripts/TowerBehaviourScript.cs
+++ TowerNinja/Assets/Scripts/TowerBehaviourScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class TowerBehaviourScript : MonoBehaviour
 {
+    private int health_point;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health_point = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // check health_point value
+            
+            // update tower appearance
     }
 }
