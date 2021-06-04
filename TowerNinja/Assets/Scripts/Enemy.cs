4286856987eb41ddca6029778f394a1d45f89a98
Philip Jung <philipju@usc.edu>
2021-06-03 21:01:12 -0700

    Switched enemy and enemyspawn scripts. Added getcomponent script to enemypsawn

--- TowerNinja/Assets/Scripts/Enemy.cs
+++ TowerNinja/Assets/Scripts/Enemy.cs
@@ -6,32 +6,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform prefab;
-    public Vector3 velocity;
-    public Transform enemy;
-    public GameObject go;
-
-    void Start()
-    {
-    	velocity.x = 1.5f;
-        velocity.y = 0;
-        velocity.z = 0;
-
-        enemy = Instantiate(prefab, new Vector3(-5, -3, 0), Quaternion.identity);
-        go = enemy.gameObject;
-        go.AddComponent<Rigidbody2D>();
-        go.GetComponent<Rigidbody2D>().gravityScale = 0;
-        go.GetComponent<Rigidbody2D>().velocity = velocity;
-
-    	//enemy.rigidbody.velocity = velocity;
-    }
-    void Update()
-    {
-    	//transform.Translate(velocity*Time.deltaTime, Space.World);
-    	if (enemy.position.x >= 5) {
-    		go.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-    	}
-    }
+    
 }
 
 
