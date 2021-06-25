91ac85ff08409106f29f6e3dc1748d196d311c38
SKT T1 IU <32501438+mathpop09@users.noreply.github.com>
2021-06-24 16:54:53 -1000

    Cleaned up PR

--- TowerNinja/Assets/Scripts/FireballSpawn.cs
+++ TowerNinja/Assets/Scripts/FireballSpawn.cs
@@ -1,37 +1,37 @@
 using UnityEngine;
 using System.Collections.Generic;
- 
+
 public class FireballSpawn : MonoBehaviour
 {
 	// Color of the gizmo
 	public Color gizmoColor = Color.red;
- 
+
 	//-----------------------------------
 	// All the Enums
 	//-----------------------------------
 	// Spawn types
 	public enum SpawnTypes
-    {
+	{
 		Normal,
 		Once,
 		Wave,
 		TimedWave
-    }
+	}
 	// The different Enemy levels
 	public enum EnemyLevels
-    {
+	{
 		Easy,
 		Medium,
 		Hard,
 		Boss
-    }
+	}
 	//---------------------------------
 	// End of the Enums
 	//---------------------------------
- 
+
 	// Enemy level to be spawnedEnemy
 	public EnemyLevels enemyLevel = EnemyLevels.Easy;
- 
+
 	//----------------------------------
 	// Enemy Prefabs
 	//----------------------------------
@@ -45,7 +45,7 @@ public class FireballSpawn : MonoBehaviour
 	//----------------------------------
 	// End of Enemy Prefabs
 	//----------------------------------
- 
+
 	//----------------------------------
 	// Enemies and how many have been created and how many are to be created
 	//----------------------------------
@@ -55,11 +55,11 @@ public class FireballSpawn : MonoBehaviour
 	//----------------------------------
 	// End of Enemy Settings
 	//----------------------------------
- 
- 
+
+
 	// The ID of the spawner
 	private int SpawnID;
- 
+
 	//----------------------------------
 	// Different Spawn states and ways of doing them
 	//----------------------------------
@@ -72,7 +72,6 @@ public class FireballSpawn : MonoBehaviour
 	//Wave controls
 	public int totalWaves = 5;
 	private int numWaves = 0;
-
 	//----------------------------------
 	// End of Different Spawn states and ways of doing them
 	//----------------------------------
@@ -92,19 +91,105 @@ public class FireballSpawn : MonoBehaviour
 		// Sets the color to red
 		Gizmos.color = gizmoColor;
 		//draws a small cube at the location of the game object that the script is attached to
-		Gizmos.DrawCube(transform.position, new Vector3 (0.5f,0.5f,0.5f));
+		Gizmos.DrawCube(transform.position, new Vector3(0.5f, 0.5f, 0.5f));
 	}
-	void Update ()
+	void Update()
 	{
-		if(Spawn)
+		if (Spawn)
 		{
-			
+			lastSpawnTime += Time.deltaTime;
+			// Spawns enemies everytime one dies
+			if (spawnType == SpawnTypes.Normal && lastSpawnTime > 3)
+			{
+				// checks to see if the number of spawned enemies is less than the max num of enemies
+				if (numEnemy < totalEnemy)
+				{
+					// spawns an enemy
+					spawnEnemy();
+					lastSpawnTime = 0;
+				}
+			}
+			// Spawns enemies only once
+			else if (spawnType == SpawnTypes.Once)
+			{
+				// checks to see if the overall spawned num of enemies is more or equal to the total to be spawned
+				if (spawnedEnemy >= totalEnemy)
+				{
+					//sets the spawner to false
+					Spawn = false;
+				}
+				else
+				{
+					// spawns an enemy
+					spawnEnemy();
+				}
+			}
+			//spawns enemies in waves, so once all are dead, spawns more
+			else if (spawnType == SpawnTypes.Wave)
+			{
+				if (numWaves < totalWaves + 1)
+				{
+					if (waveSpawn)
+					{
+						//spawns an enemy
+						spawnEnemy();
+					}
+					if (numEnemy == 0)
+					{
+						// enables the wave spawner
+						waveSpawn = true;
+						//increase the number of waves
+						numWaves++;
+					}
+					if (numEnemy == totalEnemy)
+					{
+						// disables the wave spawner
+						waveSpawn = false;
+					}
+				}
+			}
+			// Spawns enemies in waves but based on time.
+			else if (spawnType == SpawnTypes.TimedWave)
+			{
+				// checks if the number of waves is bigger than the total waves
+				if (numWaves <= totalWaves)
+				{
+					// Increases the timer to allow the timed waves to work
+					timeTillWave += Time.deltaTime;
+					if (waveSpawn)
+					{
+						//spawns an enemy
+						spawnEnemy();
+					}
+					// checks if the time is equal to the time required for a new wave
+					if (timeTillWave >= waveTimer)
+					{
+						// enables the wave spawner
+						waveSpawn = true;
+						// sets the time back to zero
+						timeTillWave = 0.0f;
+						// increases the number of waves
+						numWaves++;
+						// A hack to get it to spawn the same number of enemies regardless of how many have been killed
+						numEnemy = 0;
+					}
+					if (numEnemy >= totalEnemy)
+					{
+						// diables the wave spawner
+						waveSpawn = false;
+					}
+				}
+				else
+				{
+					Spawn = false;
+				}
+			}
 		}
 	}
 	// spawns an enemy based on the enemy level that you selected
 	private void spawnEnemy()
 	{
-		GameObject Enemy = (GameObject) Instantiate(Enemies[enemyLevel], gameObject.transform.position, Quaternion.identity);
+		GameObject Enemy = (GameObject)Instantiate(Enemies[enemyLevel], gameObject.transform.position, Quaternion.identity);
 		Enemy.SendMessage("setName", SpawnID);
 		// Increase the total number of enemies spawned and the number of spawned enemies
 		numEnemy++;
@@ -130,7 +215,7 @@ public class FireballSpawn : MonoBehaviour
 	//disable the spawner based on spawnerID
 	public void disableSpawner(int sID)
 	{
-		if(SpawnID == sID)
+		if (SpawnID == sID)
 		{
 			Spawn = false;
 		}
--- TowerNinja/Assets/Scripts/enemyDmgHitbox.cs
+++ TowerNinja/Assets/Scripts/enemyDmgHitbox.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class enemyDmgHitbox : MonoBehaviour
 {
+    private Rigidbody2D thisRB;
     public  int MaxHealthPoint = 50;
     private static readonly int MinHealthPoint = 0;
     private int _healthPoint;
@@ -18,6 +19,7 @@ public class enemyDmgHitbox : MonoBehaviour
     private Vector2 velocity;
     private int totalCollisions;
     public float Velocity;
+    
     void Start()
     {
         totalCollisions = 0;
@@ -32,7 +34,8 @@ public class enemyDmgHitbox : MonoBehaviour
         }
         //damageTime = 1.5f;
         damageTimer = 0.0f;
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(Velocity, 0);
+        thisRB = this.GetComponent<Rigidbody2D>();
+        thisRB.velocity = new Vector2(Velocity, 0);
         EnemyWave.spawnedEnemy++;
     }
     void Update()
@@ -53,7 +56,7 @@ public class enemyDmgHitbox : MonoBehaviour
         if (totalCollisions == 0)
         {
             damageBool = false;
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(Velocity, 0);
+            thisRB.velocity = new Vector2(Velocity, 0);
         }
         //transform.Translate(velocity*Time.deltaTime, Space.World);
         /*
@@ -70,52 +73,19 @@ public class enemyDmgHitbox : MonoBehaviour
         if (collision.tag == "friendly")
         {
             totalCollisions++;
-            //Debug.Log("proc");
             damageBool = true;
-            //Debug.Log(collision.gameObject);
-            //Debug.Log(collision.gameObject.transform.parent);
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+            thisRB.velocity = new Vector2(0.0f, 0.0f);
         }
-            //Die();
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        //Debug.Log("stay");
-        //Debug.Log(damageTimer);
-        /*
-        if (collision.tag == "friendly")
-        {
-            //Debug.Log(collision.gameObject);
-            //Debug.Log(damageTimer);
-            damageBool = true; 
-        }
-        if (damageTimer >= damageTime) // && collision.tag == "friendly"
-        {
-            if (collision.tag == "friendly")
-            {
-                damageTimer = 0;
-                
-                Debug.Log(collision.gameObject);
-                Debug.Log(collision.gameObject.transform.parent);
-                
-                collision.gameObject.SendMessage("DamageTower", damage);
-
-                Debug.Log(damage);
-                collision.gameObject.transform.parent.SendMessage("DamageFriendly", 10);
-            }
-            
-        }
-        */
         if (damageTimer >= damageTime && totalCollisions > 0 && collision.gameObject.tag == "friendly")
         {
             damageBool = true;
             damageTimer = 0;
-            //if (collision.gameObject.tag == "friendly")
-            //{
-                collision.gameObject.SendMessage("DamageTower", damage);
-                collision.gameObject.transform.parent.SendMessage("DamageFriendly", damage);
-            //}
+            collision.gameObject.SendMessage("DamageTower", damage);
+            collision.gameObject.transform.parent.SendMessage("DamageFriendly", damage);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -123,12 +93,10 @@ public class enemyDmgHitbox : MonoBehaviour
         if (collision.tag == "friendly")
         {
             totalCollisions--;
-            //damageTimer = 0;
-            //Debug.Log("leave");
             if (totalCollisions == 0)
             {
                 damageBool = false;
-                this.GetComponent<Rigidbody2D>().velocity = new Vector2(Velocity, 0);
+                thisRB.velocity = new Vector2(Velocity, 0);
             }
         }
     }
