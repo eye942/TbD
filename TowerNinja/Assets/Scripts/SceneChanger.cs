72622cbe8193272c7331f7c4fae6073f1f2800c3
sway2020 <ianyisui@gmail.com>
2021-07-13 19:54:29 -0700

    Add new buttons on the entry sceen

--- TowerNinja/Assets/Scripts/SceneChanger.cs
+++ TowerNinja/Assets/Scripts/SceneChanger.cs
@@ -4,10 +4,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger: MonoBehaviour
 {
-    public static void PlayGame()
+    public static void PlayEasyGame()
     {
+        BalanceManager.FireballMaxClicks = BalanceManager.FireballMaxClicksEasy;
+        Debug.Log("Set difficulty to - Easy");
         SceneManager.LoadScene("Scenes/MainGame");
         AnalyticsEvent.GameStart();
+    }
+    public static void PlayNormalGame()
+    {
+        BalanceManager.FireballMaxClicks = BalanceManager.FireballMaxClicksNormal;
+        Debug.Log("Set difficulty to - Normal");
+        SceneManager.LoadScene("Scenes/MainGame");
+        AnalyticsEvent.GameStart();
+    }
+    public static void PlayHardGame()
+    {
+        BalanceManager.FireballMaxClicks = BalanceManager.FireballMaxClicksHard;
+        Debug.Log("Set difficulty to - Hard");
+        SceneManager.LoadScene("Scenes/MainGame");
+        AnalyticsEvent.GameStart();
+    }
+    public static void ReplayGame()
+    {
+        if (BalanceManager.FireballMaxClicks == BalanceManager.FireballMaxClicksEasy) PlayEasyGame();
+        else if (BalanceManager.FireballMaxClicks == BalanceManager.FireballMaxClicksNormal) PlayHardGame();
+        else if (BalanceManager.FireballMaxClicks == BalanceManager.FireballMaxClicksHard) PlayNormalGame();
     }
     public static void GameOver()
     {
