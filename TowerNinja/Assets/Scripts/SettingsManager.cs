6653124e031468c1cbeeb169026775a39addc79a
sway2020 <ysui@usc.edu>
2021-07-19 14:17:09 -0700

    Add mute & back buttons to mainGame

--- TowerNinja/Assets/Scripts/SettingsManager.cs
+++ TowerNinja/Assets/Scripts/SettingsManager.cs
@@ -1,24 +1,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
     public static bool AudioStateOn = true;
 
-    void Start()
-    {
-        HashSet<string> scenesWithBackgroundMusic = new HashSet<string>()
+    private static readonly HashSet<string> scenesWithBackgroundMusic = new HashSet<string>()
         {
             "EntryScreen",
             "MainGame",
             "GameOverScreen"
         };
 
+    private static readonly HashSet<string> scenesWithGameMuteButton = new HashSet<string>()
+        {
+            "MainGame"
+        };
+
+    void Start()
+    {
+        UpdateBackgroundAudio();
+    }
+    private static void UpdateBackgroundAudio()
+    {
         if (scenesWithBackgroundMusic.Contains(SceneManager.GetActiveScene().name))
         {
             AudioSource backgroundAudio = GameObject.Find("BackgroundAudio").GetComponent<AudioSource>();
             backgroundAudio.mute = !AudioStateOn;
         }
+    }
+
+    public static void ChangeAudioState()
+    {
+        AudioStateOn = !AudioStateOn;
+
+        if (scenesWithGameMuteButton.Contains(SceneManager.GetActiveScene().name))
+        {
+            Text text = GameObject.Find("GameMuteButtonText").GetComponent<Text>();
+            if (text.text == "Mute") text.text = "Unmute";
+            else if (text.text == "Unmute") text.text = "Mute";
+        }
+
+        UpdateBackgroundAudio();
     }
 }
