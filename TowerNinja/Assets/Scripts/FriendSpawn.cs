bafc571d4174aceb700e8e240d3f3a90d21da65e
Chris Glover <chris.glover@ridgelineapps.com>
2021-07-21 01:11:55 -0400

    time gating and UI

--- TowerNinja/Assets/Scripts/FriendSpawn.cs
+++ TowerNinja/Assets/Scripts/FriendSpawn.cs
@@ -14,6 +14,7 @@ public class FriendSpawn : MonoBehaviour
 
     private ResourceManager resourceManager;
     private int _friendManaCost;
+    private int spawnCode;   //std = 1, big = 2, slinger = 3
 
     void Start()
     {
@@ -22,9 +23,19 @@ public class FriendSpawn : MonoBehaviour
         resourceManager = resourceManagerObject.GetComponent<ResourceManager>();
 
         // set mana cost for this spawner
-        if (prefab.name == "BigFriend") _friendManaCost = BalanceManager.ManaBigFriendCost;
-        else if (prefab.name == "FriendSlinger") _friendManaCost = BalanceManager.ManaFriendSlingerCost;
-        else _friendManaCost = BalanceManager.ManaFriendCost;
+        if (prefab.name == "BigFriend") {
+            _friendManaCost = BalanceManager.ManaBigFriendCost;
+            spawnCode= 2;
+        }
+        else if (prefab.name == "FriendSlinger"){
+             _friendManaCost = BalanceManager.ManaFriendSlingerCost;
+             spawnCode= 3;
+        }
+        else {
+            _friendManaCost = BalanceManager.ManaFriendCost;
+            spawnCode= 1;
+            }
+
 
         /*
         friend = Instantiate(prefab, this.transform.position, Quaternion.identity) ;
@@ -46,8 +57,9 @@ public class FriendSpawn : MonoBehaviour
         if (Input.GetKeyDown(activateKey))
         {
             // check if we have enough mana to spawn new friendly units
-            if (HaveEnoughMana())
+            if (HaveEnoughMana()&& CanSpawn())
             {
+                Spawn();
                 friend = Instantiate(prefab, this.transform.position, Quaternion.identity);
                 resourceManager.DecreaseMana(_friendManaCost);
                 //Debug.Log("lets go");
@@ -62,6 +74,24 @@ public class FriendSpawn : MonoBehaviour
     {
         if (resourceManager.GetManaCount() >= _friendManaCost) return true;
         return false;
+    }
+
+    private bool CanSpawn()
+    {
+            if (spawnCode == 1)
+                return resourceManager.CanSpawnFriendly();
+            if(spawnCode== 2)
+                return resourceManager.CanSpawnBigFriendly();
+            return resourceManager.CanSpawnSlingerFriendly();
+    }
+    private void Spawn()
+    {
+            if (spawnCode == 1)
+                resourceManager.SpawnFriendly();
+            else if(spawnCode== 2)
+                resourceManager.SpawnBigFriendly();
+            else
+                resourceManager.SpawnSlingerFriendly();
     }
 }
 
